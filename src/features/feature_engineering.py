ba3e94cb5cab057b8b42fe2f44e18a41f310c2be
Samarth Gite <76243547+Samarth777415@users.noreply.github.com>
2025-06-12 21:07:15 +0530

    CI-5

--- src/features/feature_engineering.py
+++ src/features/feature_engineering.py
@@ -2,13 +2,13 @@
 import numpy as np
 import pandas as pd
 import os
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import yaml
 from src.logger import logging
 import pickle
 
+
 def load_params(params_path: str) -> dict:
-    """Load parameters from a YAML file."""
     try:
         with open(params_path, 'r') as file:
             params = yaml.safe_load(file)
@@ -24,8 +24,8 @@ def load_params(params_path: str) -> dict:
         logging.error('Unexpected error: %s', e)
         raise
 
+
 def load_data(file_path: str) -> pd.DataFrame:
-    """Load data from a CSV file."""
     try:
         df = pd.read_csv(file_path)
         df.fillna('', inplace=True)
@@ -38,36 +38,37 @@ def load_data(file_path: str) -> pd.DataFrame:
         logging.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-def apply_tfidf(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: int) -> tuple:
-    """Apply TF-IDF Vectorizer to the data."""
+
+def apply_bow(train_data: pd.DataFrame, test_data: pd.DataFrame, max_features: int) -> tuple:
     try:
-        logging.info("Applying TF-IDF Vectorizer...")
-        vectorizer = TfidfVectorizer(max_features=max_features)
+        logging.info("Applying BOW...")
+        vectorizer = CountVectorizer(max_features=max_features)
 
         X_train = train_data['review'].values
         y_train = train_data['sentiment'].values
         X_test = test_data['review'].values
         y_test = test_data['sentiment'].values
 
-        X_train_tfidf = vectorizer.fit_transform(X_train)
-        X_test_tfidf = vectorizer.transform(X_test)
+        X_train_bow = vectorizer.fit_transform(X_train)
+        X_test_bow = vectorizer.transform(X_test)
 
-        train_df = pd.DataFrame(X_train_tfidf.toarray())
+        train_df = pd.DataFrame(X_train_bow.toarray())
         train_df['label'] = y_train
 
-        test_df = pd.DataFrame(X_test_tfidf.toarray())
+        test_df = pd.DataFrame(X_test_bow.toarray())
         test_df['label'] = y_test
 
+        os.makedirs('models', exist_ok=True)
         pickle.dump(vectorizer, open('models/vectorizer.pkl', 'wb'))
-        logging.info('TF-IDF Vectorizer applied and data transformed')
+        logging.info('Bag of Words applied and data transformed')
 
         return train_df, test_df
     except Exception as e:
-        logging.error('Error during TF-IDF transformation: %s', e)
+        logging.error('Error during Bag of Words transformation: %s', e)
         raise
 
+
 def save_data(df: pd.DataFrame, file_path: str) -> None:
-    """Save the dataframe to a CSV file."""
     try:
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
         df.to_csv(file_path, index=False)
@@ -75,6 +76,7 @@ def save_data(df: pd.DataFrame, file_path: str) -> None:
     except Exception as e:
         logging.error('Unexpected error occurred while saving the data: %s', e)
         raise
+
 
 def main():
     try:
@@ -84,13 +86,16 @@ def main():
         train_data = load_data('./predata/interim/train_processed.csv')
         test_data = load_data('./predata/interim/test_processed.csv')
 
-        train_df, test_df = apply_tfidf(train_data, test_data, max_features)
+        train_df, test_df = apply_bow(train_data, test_data, max_features)
 
         save_data(train_df, os.path.join("./predata", "processed", "train_tfidf.csv"))
-        save_data(test_df, os.path.join("./predata", "processed", "test_tfidf.csv"))
+        save_data(test_df, os.path.join("./predata", "processed", "test_tfidi.csv"))
+
+        logging.info("Feature engineering completed successfully.")
     except Exception as e:
         logging.error('Failed to complete the feature engineering process: %s', e)
         print(f"Error: {e}")
+
 
 if __name__ == '__main__':
     main()
